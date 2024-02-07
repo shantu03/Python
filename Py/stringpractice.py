3328145f42c660ea0e68e37b0a6de5a63eaee140
Shantanu Hulwan <117788434+shantu03@users.noreply.github.com>
2024-02-07 16:58:48 +0530

    Create stringpractice.py

---
+++ Py/stringpractice.py
@@ -0,0 +1,24 @@
+#chapter 11 -- page 4
+#chapter 12 -- page 4 & page 5
+
+string=''' hello \
+this is me \
+and that is that '''
+
+string2='''12345\
+12345 12345'''
+        
+print(len(string2))
+
+for i in string2:
+    print(i)
+    
+    
+print("chapter 12\n")
+
+string3 ='I am shantanu hulwan'
+print(string3[5:14])
+
+string4="shantanu"
+print(string4[-4:-2])
+print("last - "+string4[-8:-0])
