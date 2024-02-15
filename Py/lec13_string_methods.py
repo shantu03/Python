13a08d9567dff40f478efdbbe9c72d9a051e739f
Shantanu <hulwanshantanu@gmai.com>
2024-02-15 10:22:32 +0530

    in kali

---
+++ Py/lec13_string_methods.py
@@ -0,0 +1,14 @@
+a="my name is shantanu hulwan.i am from kaRad !!!. nice to meet you"
+print(a)
+print("upper - "+a.upper())
+print("lower- "+a.lower())
+print("spliting characters ")
+print(a.split())
+print(a.rstrip("uoy"))
+print(a.capitalize())
+b="SHANTANU"
+print(b.center(30))
+print("normal length - ",len(b))
+print("center method length - ",len(b.center(10)))
+
+print(b.count("n"))
